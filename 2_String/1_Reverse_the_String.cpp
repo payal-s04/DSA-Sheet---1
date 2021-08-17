5ba581696d90150d134a40e8909b6720ea71f0d2
payal-s04 <87601475+payal-s04@users.noreply.github.com>
2021-08-17 20:07:19 +0530

    Update 1_Reverse_the_String.cpp

--- 2_String/1_Reverse_the_String.cpp
+++ 2_String/1_Reverse_the_String.cpp
@@ -1,1 +1,48 @@
+/*
+Problem 1 :
 
+You are given t number of testcases. Each testcase contains a string s.
+Write a program to reverse the given array string.
+
+E.g.
+ Input:
+ github
+ 
+ Output:
+ buhtig
+*/
+
+#include <iostream>
+using namespace std;
+
+void reversedString(string s)
+{
+    int len = s.size(), i = 0;
+  
+    while (i < len)
+    {
+        char temp = s[len - 1];
+        s[len - 1] = s[i];
+        s[i] = temp;
+        len--;
+        i++;
+    }
+  
+    cout << s << endl;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        string s;
+        cin >> s;
+
+        reversedString(s);
+    }
+
+    return 0;
+}
